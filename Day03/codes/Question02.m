a2ca99782386da8e1287cb457b660a5730eaacbe
MrManoj124 <manojmanorooban@gmail.com>
2025-04-18 00:35:08 -0700

    created a file as Question02.m

---
+++ Day03/codes/Question02.m
@@ -0,0 +1,12 @@
+% Find the Largest number 
+x = input('Enter the number 1 : ')
+y = input('Enter the number 2 : ')
+z = input('Enter the number 3 : ')
+
+if(x>y && x>z)
+    disp("x")
+elseif(y>x && y>z)
+    disp("y")
+else
+    disp("z")
+end
